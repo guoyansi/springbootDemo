7c0aaca9c94ddfdd5eda5d3e8bb373790a958119
guoys <2835127910@qq.com>
2020-01-06 10:15:17 +0800

    添加thymeleaf的公共文件

--- src/main/java/com/gys/item/interceptor/ExceptionInterceptor.java
+++ src/main/java/com/gys/item/interceptor/ExceptionInterceptor.java
@@ -1,12 +1,18 @@
 package com.gys.item.interceptor;
 
+import com.gys.item.ex.MyAjaxException;
+import com.gys.item.ex.MyAjaxNoSessionException;
 import com.gys.item.ex.MyViewException;
 import com.gys.item.ex.MyViewNoSessionException;
 import com.gys.item.util.HttpResult;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @ControllerAdvice
 public class ExceptionInterceptor {
@@ -17,14 +23,15 @@ public class ExceptionInterceptor {
         e.printStackTrace();
         return HttpResult.errorEx();
     }
+
     @ResponseBody
-    @ExceptionHandler({MyException.class})
-    public HttpResult myExceptionHandler(MyException e){
+    @ExceptionHandler({MyAjaxException.class})
+    public HttpResult myExceptionHandler(MyAjaxException e){
         return HttpResult.error(e.getMessage());
     }
     @ResponseBody
-    @ExceptionHandler({MyNoSessionException.class})
-    public HttpResult myNoSessionExceptionHandler(MyNoSessionException e){
+    @ExceptionHandler({MyAjaxNoSessionException.class})
+    public HttpResult myNoSessionExceptionHandler(MyAjaxNoSessionException e){
         return HttpResult.noSessionEx();
     }
 
@@ -33,18 +40,31 @@ public class ExceptionInterceptor {
         ModelAndView view=new ModelAndView();
         view.addObject("msg",e.getMessage());
         if(e.getHref()==null){
-            view.setViewName("expage");
+            view.setViewName("/public/exPage");
         }else{
             view.setViewName(e.getHref());
         }
-
         return view;
     }
     @ExceptionHandler({MyViewNoSessionException.class})
-    public ModelAndView myViewNoSessionException(MyNoSessionException e){
+    public ModelAndView myViewNoSessionException(MyViewNoSessionException e){
         ModelAndView view=new ModelAndView();
         view.addObject("msg",e.getMessage());
-        view.setViewName("expage");
+        view.setViewName(e.getHref()==null?"/public/noSession":e.getHref());
         return view;
+    }
+
+    /**
+     * 每个页面都能访问到这里的属性
+     * @param model
+     */
+    @ModelAttribute
+    public void addAttribute(Model model, HttpSession session){
+        Object objName=session.getAttribute("username");
+        String username=null;
+        if(objName!=null){
+            username=objName.toString();
+        }
+        model.addAttribute("test",username);
     }
 }
